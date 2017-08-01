7f24020a05a32b1da0aacd82efcb544517b2b2fe
WANGZHIGUO1 <wangzhiguo1@kingsoft.com>
2017-08-01 21:27:54 +0800

    update

--- inception2.py
+++ inception2.py
@@ -6,7 +6,7 @@ from utils import num_channels, num_labels, image_size
 import trainer
 
 keep_prob = 0.5
-reduce1 = 8
+reduce1 = 16
 reduce2 = 16
 map1 = 32
 map2 = 64
@@ -39,29 +39,26 @@ def create_inception_vars(in_num, reduce1x1, mapn):
     W1x1_4 = create_weight([1, 1, in_num, mapn])
     W1x3 = create_weight([1, 3, reduce1x1, reduce1x1])
     W3x1 = create_weight([3, 1, reduce1x1, mapn])
-    W1x5 = create_weight([1, 5, reduce1x1, reduce1x1])
-    W5x1 = create_weight([5, 1, reduce1x1, mapn])
-    B1x1 = tf.Variable(tf.zeros([mapn]))
+    W1x3_1 = create_weight([1, 3, reduce1x1, reduce1x1])
+    W3x1_2 = create_weight([3, 1, reduce1x1, reduce1x1])
+    W1x3_3 = create_weight([1, 3, reduce1x1, reduce1x1])
+    W3x1_4 = create_weight([3, 1, reduce1x1, mapn])
     B1x1_2 = tf.Variable(tf.zeros([reduce1x1]))
     B1x1_3 = tf.Variable(tf.zeros([reduce1x1]))
-    B3x3 = tf.Variable(tf.zeros([mapn]))
-    B5x5 = tf.Variable(tf.zeros([mapn]))
-    B1x1_4 = tf.Variable(tf.zeros([mapn]))
-    lst = (W1x1_1, W1x1_2, W1x1_3, W1x1_4, W1x3, W3x1, W1x5, W5x1, B1x1,
-           B1x1_2, B1x1_3, B1x1_4, B3x3, B5x5)
+    B1 = tf.Variable(tf.zeros([mapn*4]))
+    lst = (W1x1_1, W1x1_2, W1x1_3, W1x1_4, W1x3, W3x1, W1x3_1, W3x1_2, W1x3_3, W3x1_4, B1, B1x1_2, B1x1_3)
     return lst
 
 
 def model_inception(data, *args):
-    (W1x1_1, W1x1_2, W1x1_3, W1x1_4, W1x3, W3x1, W1x5, W5x1, B1x1, B1x1_2,
-     B1x1_3, B1x1_4, B3x3, B5x5) = args
-    conv1 = conv2d_s(data, W1x1_1, s=2) + B1x1
-    conv2 = tf.nn.relu(conv2d_s(data, W1x1_2, s=2) + B1x1_2)
-    conv3x3 = conv2d_s(conv2d_s(conv2, W1x3, s=1), W3x1, s=1) + B3x3
-    conv3 = tf.nn.relu(conv2d_s(data, W1x1_3, s=2) + B1x1_3)
-    conv5x5 = conv2d_s(conv2d_s(conv3, W1x5, s=1), W5x1, s=1) + B5x5
-    conv4 = conv2d_s(max_pool_3x3_s(data, s=2), W1x1_4, s=1) + B1x1_4
-    incept = tf.nn.relu(tf.concat([conv1, conv3x3, conv5x5, conv4], axis=3))
+    (W1x1_1, W1x1_2, W1x1_3, W1x1_4, W1x3, W3x1, W1x3_1, W3x1_2, W1x3_3, W3x1_4, B1, B1x1_2, B1x1_3) = args
+    conv1 = conv2d_s(data, W1x1_1, s=2)
+    conv2 = tf.nn.relu(conv2d_s(data, W1x1_2) + B1x1_2)
+    conv3x3 = conv2d_s(conv2d_s(conv2, W1x3), W3x1, s=2)
+    conv3 = tf.nn.relu(conv2d_s(data, W1x1_3) + B1x1_3)
+    conv5x5 = conv2d_s(conv2d_s(conv2d_s(conv2d_s(conv3, W1x3_1), W3x1_2), W1x3_3), W3x1_4, s=2)
+    conv4 = conv2d_s(max_pool_3x3_s(data, s=2), W1x1_4)
+    incept = tf.nn.relu(tf.concat([conv1, conv3x3, conv5x5, conv4], axis=3) + B1)
     print incept.shape
     return incept
 
@@ -85,11 +82,10 @@ with graph.as_default():
 
     incept_lst1 = create_inception_vars(num_channels, reduce1, map1)
     incept_lst2 = create_inception_vars(4 * map1, reduce2, map2)
-    Wfc_1 = create_weight([image_size * image_size * map2 / 4, num_fc1])
+    Wfc_1 = create_weight([image_size * image_size * map2 /4, num_fc1])
     Bfc_1 = create_bias([num_fc1])
-    Wfc_2 = create_weight([image_size, image_size, map2])
-    Wfc_2 = tf.Variable(tf.truncated_normal([num_fc1, num_labels], stddev=0.1))
+    Wfc_2 = create_weight([num_fc1, num_labels])
     Bfc_2 = create_bias([num_labels])
 
 # Test accuracy: 93.2% after 1001 steps
-trainer.run(graph, model, batch_size=50, num_steps=1001)
+trainer.run(graph, model, num_steps=401, use_previous=1)
--- trainer.py
+++ trainer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
 import tensorflow as tf
 from utils import accuracy, image_size, num_channels, num_labels, load_pickle, clean_dulplicate
 
 
-def run(graph, model, batch_size=16, num_steps=1001):
+def run(graph, model, batch_size=16, num_steps=1001,
+        model_path = './model.ckpt', use_previous=1):
     pickle_file = 'notMNIST.pickle'
     dic = load_pickle(pickle_file, twoD=True)
     clean_dulplicate(dic)
@@ -31,12 +33,18 @@ def run(graph, model, batch_size=16, num_steps=1001):
         valid_prediction = tf.nn.softmax(model(valid_dataset, training=False))
         test_prediction = tf.nn.softmax(model(test_dataset, training=False))
 
+        init = tf.global_variables_initializer()
+        #use to save variables so we can pick up later
+        saver = tf.train.Saver()
+
     with tf.Session(graph=graph) as session:
-        # This is a one-time operation which ensures the parameters get initialized as
-        # we described in the graph: random weights for the matrix, zeros for the
-        # biases.
-        tf.global_variables_initializer().run()
-        print('Initialized')
+        #initialize variables
+        session.run(init)
+
+        if use_previous and os.path.isfile(model_path+'.index'):
+            print 'restore %s' %model_path
+            saver.restore(session, model_path)
+
         for step in range(num_steps):
             offset = (step * batch_size) % (
                 dic['train_labels'].shape[0] - batch_size)
@@ -51,3 +59,5 @@ def run(graph, model, batch_size=16, num_steps=1001):
                 print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
                 print('Validation accuracy: %.1f%%' % accuracy(valid_prediction.eval(), dic['valid_labels']))
         print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), dic['test_labels']))
+
+        saver.save(session, model_path)
